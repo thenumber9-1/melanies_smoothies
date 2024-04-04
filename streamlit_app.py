dacbfa42c0a6b3423ddb58aa2acfe6e4b686ae37
thenumber9-1 <166075583+thenumber9-1@users.noreply.github.com>
2024-04-04 17:28:26 -0600

    bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write('The name on your Smoothie witll be: ', name_on_order)
 
 cxn = st.connection("snowflake")
 session = cxn.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
